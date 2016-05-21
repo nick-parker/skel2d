9b6dd7c91fcc2da3495687a45e9a18e166dbaa5c
Nick Parker <nmp67@cornell.edu>
2016-05-21 16:00:46 -0400

    Fixed edge ordering
    Fixed a parenthesis problem in the distance calcs, then fixed
    the Ordering of Event.class to make the skeleton handle them
    in the right order. Also added a cleaner for the Step X files.

--- src/debug/Main.scala
+++ src/debug/Main.scala
@@ -1,12 +1,18 @@
 package debug
 
+import java.io.File
 import util.Vec
 import io.ReadFile
 import display.{Draw, Line}
-import skeleton.{Skeleton, Corner}
+import skeleton.{Skeleton, Corner, Edge}
 
 object Main extends App{
-  override def main(args:Array[String]){
+	override def main(args:Array[String]){
+	  for(f <- (new File(".")).listFiles){
+	    if(f.toString().startsWith("./Step")){
+	      f.delete()
+	    }
+	  }
 		val filename = "points.txt"
 		val pnts = ReadFile.read(filename)
 		println(pnts)
@@ -17,6 +23,13 @@ object Main extends App{
 		print(sk.nodes.length)
 		val output = "skel.png"
 		Draw.draw_lines(sk.get_display(), sk.max.x - sk.min.x, sk.max.y - sk.min.y, 100, output)
-//		Draw.draw_lines(sk.get_bisectors(), sk.max.x -  sk.min.x, sk.max.y - sk.min.y, 100, "bisectors.png")
+		Draw.draw_lines(sk.get_bisectors(), sk.max.x -  sk.min.x, sk.max.y - sk.min.y, 100, "bisectors.png")
+	}
+	def test_edges() {
+		val c1 = new Corner(0, 0)
+		val c2 = new Corner(5, 0)
+		val p = new Vec(3,2)
+		val e1 = new Edge(c1, c2)
+		println(e1.distance(p))
 	}
 }
--- src/skeleton/Edge.scala
+++ src/skeleton/Edge.scala
@@ -8,6 +8,6 @@ class Edge(val a: Corner, val b: Corner) {
     return b-a
   }
   def distance(p: TwoD): Double =  {
-    abs((b.y - a.y)*p.x - (b.x - a.x)*p.y + b.x*a.y - b.y*a.x / sqrt(pow(b.y - a.y, 2) + pow(b.x - a.x, 2)))
+    abs((b.y - a.y)*p.x - (b.x - a.x)*p.y + b.x*a.y - b.y*a.x) / sqrt(pow(b.y - a.y, 2) + pow(b.x - a.x, 2))
   }
 }
