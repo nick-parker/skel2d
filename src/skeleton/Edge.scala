824fa1727497a4f62c0b60db33c8d540d8a29388
Nick Parker <nmp67@cornell.edu>
2016-05-21 15:30:36 -0400

    Small change to distance calculations

--- src/skeleton/Edge.scala
+++ src/skeleton/Edge.scala
@@ -1,14 +1,13 @@
 package skeleton
 
 import util.{Vec, TwoD}
+import scala.math.{pow, abs, sqrt}
 
 class Edge(val a: Corner, val b: Corner) {
   def asVec(): Vec = {
     return b-a
   }
   def distance(p: TwoD): Double =  {
-    Math.abs((b.y - a.y)*p.x - (b.x - a.x)*p.y + b.x*a.y - 
-              b.y*a.x / Math.sqrt(Math.pow(b.y - a.y, 2) +
-              Math.pow(b.x - a.x, 2)))
+    abs((b.y - a.y)*p.x - (b.x - a.x)*p.y + b.x*a.y - b.y*a.x / sqrt(pow(b.y - a.y, 2) + pow(b.x - a.x, 2)))
   }
 }
